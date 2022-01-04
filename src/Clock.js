8f927d52fa0b3045481e1ee0858b80d8b0517f99
Devabhaik <dbhaik1991@gmail.com>
2022-01-04 21:17:22 +0530

    clock function done

--- src/Clock.js
+++ src/Clock.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
+import './Clock.css';
 
 class Clock extends Component {
   constructor() {
     super();
     const today = new Date(),
-      date = today.getHours() + ': ' + today.getMinutes();
+      date =
+        today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+
     this.state = { date: date };
   }
 
+  componentDidMount() {
+    setInterval(() => {
+      const currentDate = new Date(),
+        currentTime =
+          currentDate.getHours() +
+          ':' +
+          currentDate.getMinutes() +
+          ':' +
+          currentDate.getSeconds();
+
+      this.setState({ date: currentTime });
+    }, 1000);
+  }
+
   render() {
-    console.log(this.state.date);
-    return <div>Hello</div>;
+    return <div>{this.state.date}</div>;
   }
 }
 
